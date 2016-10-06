cc80d61dc78008c8187f0cb4d4f73c777b669996
doerlbh <doerlbh@gmail.com>
2016-10-05 21:09:04 -0700

    update code

---
+++ lpsolve/src/lpsolve/lpsolve.java
@@ -0,0 +1,14 @@
+package lpsolve;
+
+public class lpsolve {
+
+	public lpsolve() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
