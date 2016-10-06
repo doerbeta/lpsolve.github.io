b27b0f8f685009732536bdb36c0fc2ad4631d4ae
doerlbh <doerlbh@gmail.com>
2016-10-05 23:43:15 -0700

    update code

--- test.php
+++ test.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1>My first PHP page</h1>
-
 <?php
-echo "Hello World!";
-?> 
-
-</body>
-</html>
+if ($_GET['run']) {
+  # This code will run if ?run=true is set.
+  exec("/path/to/name.sh");
+}
+?>
